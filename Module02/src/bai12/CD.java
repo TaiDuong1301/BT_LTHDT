0a85b546be4547326742235b6713beca14d61b63
DuongTai <107740086+TaiDuong1301@users.noreply.github.com>
2022-09-14 16:30:19 +0700

    Update CD template

--- Module02/src/bai12/CD.java
+++ Module02/src/bai12/CD.java
@@ -1,58 +1,66 @@
-package bai12;
+package bai12_CD;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class CD {
     private String maCD;
     private String tuaCD;
     private int soBaiHat;
     private float giaThanh;
-
-    //Constructor
-    public CD() {
-    }
-
-    public CD(String maCD, String tuaCD, int soBaiHat, float giaThanh) {
-        this.maCD = maCD;
-        this.tuaCD = tuaCD;
-        this.soBaiHat = soBaiHat;
-        this.giaThanh = giaThanh;
-    }
-
-    //Property get
+    //Getter
     public String getMaCD() {
         return maCD;
     }
-
     public String getTuaCD() {
         return tuaCD;
     }
-
     public int getSoBaiHat() {
         return soBaiHat;
     }
-
     public float getGiaThanh() {
         return giaThanh;
     }
-
+    //Setter
     public void setMaCD(String maCD) {
         this.maCD = maCD;
     }
-
     public void setTuaCD(String tuaCD) {
         this.tuaCD = tuaCD;
     }
-
-    public void setSoBaiHat(int soBaiHat) {
-        this.soBaiHat = soBaiHat;
+    /**
+     * 
+     * @param soBaiHat
+     * @throws Exception
+     */
+    public void setSoBaiHat(int soBaiHat) throws Exception {
+        if (soBaiHat > 0) {
+            this.soBaiHat = soBaiHat;
+        } else
+            throw new Exception("1");
     }
-
     public void setGiaThanh(float giaThanh) {
+        this.giaThanh = giaThanh;
+    }
+    public CD(String maCD, String tuaCD, int soBaiHat, float giaThanh) {
+        try {
+            if (maCD != null) {
+                this.maCD = maCD;
+            }
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
+        this.tuaCD = tuaCD;
+        this.soBaiHat = soBaiHat;
         this.giaThanh = giaThanh;
     }
 
     @Override
     public String toString() {
-        return "CD [giaThanh=" + giaThanh + ", maCD=" + maCD + ", soBaiHat=" + soBaiHat + ", tuaCD=" + tuaCD + "]";
+        Locale lc = new Locale("vi", "VN");
+        NumberFormat nf = NumberFormat.getCurrencyInstance(lc);
+        String s = "";
+        s += String.format("|%10s|%10s|%10s|%10s|", getMaCD(), getTuaCD(), getSoBaiHat(), nf.format(getGiaThanh()));
+        return s;
     }
-    
 }
